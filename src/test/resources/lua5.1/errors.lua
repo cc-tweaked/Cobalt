801dce8151d0f9fb2fd6c307ac489faa019604d2
SquidDev <bonzoweb@hotmail.co.uk>
2020-01-21 20:13:53 +0000

    Reset 5.1 tests

--- src/test/resources/lua5.1/errors.lua
+++ src/test/resources/lua5.1/errors.lua
@@ -1,25 +1,24 @@
 print("testing errors")
 
-function doit(s)
-	local f, msg = loadstring(s)
-	if f == nil then return msg end
-	local cond, msg = pcall(f)
-	return (not cond) and msg
+function doit (s)
+  local f, msg = loadstring(s)
+  if f == nil then return msg end
+  local cond, msg = pcall(f)
+  return (not cond) and msg
 end
 
 
-function checkmessage(prog, msg)
-	local res = doit(prog)
-	assert(string.find(res, msg, 1, true), ("Looking for \"%s\" in %q"):format(msg, res))
+function checkmessage (prog, msg)
+  assert(string.find(doit(prog), msg, 1, true))
 end
 
-function checksyntax(prog, extra, token, line)
-	local msg = doit(prog)
-	token = string.gsub(token, "(%p)", "%%%1")
-	local pt = string.format([[^%%[string ".*"%%]:%d: .- near '%s'$]],
-		line, token)
-	assert(string.find(msg, pt), "Got " .. msg .. ", expected " .. pt)
-	assert(string.find(msg, msg, 1, true))
+function checksyntax (prog, extra, token, line)
+  local msg = doit(prog)
+  token = string.gsub(token, "(%p)", "%%%1")
+  local pt = string.format([[^%%[string ".*"%%]:%d: .- near '%s'$]],
+                           line, token)
+  assert(string.find(msg, pt))
+  assert(string.find(msg, msg, 1, true))
 end
 
 
@@ -34,34 +33,32 @@ assert(doit("error()") == nil)
 assert(doit("unpack({}, 1, n=2^30)"))
 assert(doit("a=math.sin()"))
 assert(not doit("tostring(1)") and doit("tostring()"))
-assert(doit "tonumber()")
-assert(doit "repeat until 1; a")
---checksyntax("break label", "", "label", 1)
-assert(doit ";")
-assert(doit "a=1;;")
-assert(doit "return;;")
-assert(doit "assert(false)")
-assert(doit "assert(nil)")
-assert(doit "a=math.sin\n(3)")
+assert(doit"tonumber()")
+assert(doit"repeat until 1; a")
+checksyntax("break label", "", "label", 1)
+assert(doit";")
+assert(doit"a=1;;")
+assert(doit"return;;")
+assert(doit"assert(false)")
+assert(doit"assert(nil)")
+assert(doit"a=math.sin\n(3)")
 assert(doit("function a (... , ...) end"))
 assert(doit("function a (, ...) end"))
 
---[=[
 checksyntax([[
   local a = {4
 
 ]], "'}' expected (to close '{' at line 1)", "<eof>", 3)
-]=]
 
 
 -- tests for better error messages
 
 checkmessage("a=1; bbbb=2; a=math.sin(3)+bbbb(3)", "global 'bbbb'")
 checkmessage("a=1; local a,bbbb=2,3; a = math.sin(1) and bbbb(3)",
-	"local 'bbbb'")
+       "local 'bbbb'")
 checkmessage("a={}; do local a=1 end a:bbbb(3)", "method 'bbbb'")
 checkmessage("local a={}; a.bbbb(3)", "field 'bbbb'")
-assert(not string.find(doit "a={13}; local bbbb=1; a[bbbb](3)", "'bbbb'"))
+assert(not string.find(doit"a={13}; local bbbb=1; a[bbbb](3)", "'bbbb'"))
 checkmessage("a={13}; local bbbb=1; a[bbbb](3)", "number")
 
 aaa = nil
@@ -69,14 +66,14 @@ checkmessage("aaa.bbb:ddd(9)", "global 'aaa'")
 checkmessage("local aaa={bbb=1}; aaa.bbb:ddd(9)", "field 'bbb'")
 checkmessage("local aaa={bbb={}}; aaa.bbb:ddd(9)", "method 'ddd'")
 checkmessage("local a,b,c; (function () a = b+1 end)()", "upvalue 'b'")
-assert(not doit "local aaa={bbb={ddd=next}}; aaa.bbb:ddd(nil)")
+assert(not doit"local aaa={bbb={ddd=next}}; aaa.bbb:ddd(nil)")
 
---checkmessage("b=1; local aaa='a'; x=aaa+b", "local 'aaa'") --TODO: arithmetic error on strings
+checkmessage("b=1; local aaa='a'; x=aaa+b", "local 'aaa'")
 checkmessage("aaa={}; x=3/aaa", "global 'aaa'")
 checkmessage("aaa='2'; b=nil;x=aaa*b", "global 'b'")
 checkmessage("aaa={}; x=-aaa", "global 'aaa'")
-assert(not string.find(doit "aaa={}; x=(aaa or aaa)+(aaa and aaa)", "'aaa'"))
-assert(not string.find(doit "aaa={}; (aaa or aaa)()", "'aaa'"))
+assert(not string.find(doit"aaa={}; x=(aaa or aaa)+(aaa and aaa)", "'aaa'"))
+assert(not string.find(doit"aaa={}; (aaa or aaa)()", "'aaa'"))
 
 checkmessage([[aaa=9
 repeat until 3==3
@@ -97,57 +94,56 @@ x.a()]], "field 'a'")
 checkmessage([[
 prefix = nil
 insert = nil
-while 1 do
+while 1 do  
   local a
   if nil then break end
   insert(prefix, a)
 end]], "global 'insert'")
 
---checkmessage([[  -- tail call
---  return math.sin("a")
---]], "'sin'") -- TODO?? Argument contains function name
+checkmessage([[  -- tail call
+  return math.sin("a")
+]], "'sin'")
 
 checkmessage([[collectgarbage("nooption")]], "invalid option")
 
 checkmessage([[x = print .. "a"]], "concatenate")
 
--- checkmessage("getmetatable(io.stdin).__gc()", "no value") -- No __gc method on files
+checkmessage("getmetatable(io.stdin).__gc()", "no value")
 
-print '+'
+print'+'
 
 
 -- testing line error
 
-function lineerror(s)
-	local err, msg = pcall(loadstring(s))
-	local line = string.match(msg, ":(%d+):")
-	return line and line + 0
+function lineerror (s)
+  local err,msg = pcall(loadstring(s))
+  local line = string.match(msg, ":(%d+):")
+  return line and line+0
 end
 
-assert(lineerror "local a\n for i=1,'a' do \n print(i) \n end" == 2)
-assert(lineerror "\n local a \n for k,v in 3 \n do \n print(k) \n end" == 3)
-assert(lineerror "\n\n for k,v in \n 3 \n do \n print(k) \n end" == 4)
-assert(lineerror "function a.x.y ()\na=a+1\nend" == 1)
+assert(lineerror"local a\n for i=1,'a' do \n print(i) \n end" == 2)
+assert(lineerror"\n local a \n for k,v in 3 \n do \n print(k) \n end" == 3)
+assert(lineerror"\n\n for k,v in \n 3 \n do \n print(k) \n end" == 4)
+assert(lineerror"function a.x.y ()\na=a+1\nend" == 1)
 
 local p = [[
 function g() f() end
 function f(x) error('a', X) end
 g()
 ]]
-X = 3; assert(lineerror(p) == 3)
-X = 0; assert(lineerror(p) == nil)
-X = 1; assert(lineerror(p) == 2)
-X = 2; assert(lineerror(p) == 1)
+X=3;assert(lineerror(p) == 3)
+X=0;assert(lineerror(p) == nil)
+X=1;assert(lineerror(p) == 2)
+X=2;assert(lineerror(p) == 1)
 
 lineerror = nil
 
 C = 0
-local l = debug.getinfo(1, "l").currentline; function y() C = C + 1; y() end
+local l = debug.getinfo(1, "l").currentline; function y () C=C+1; y() end
 
-local function checkstackmessage(m)
-	return (string.find(m, "^.-:%d+: stack overflow"))
+local function checkstackmessage (m)
+  return (string.find(m, "^.-:%d+: stack overflow"))
 end
-
 assert(checkstackmessage(doit('y()')))
 assert(checkstackmessage(doit('y()')))
 assert(checkstackmessage(doit('y()')))
@@ -155,19 +151,18 @@ assert(checkstackmessage(doit('y()')))
 C = 0
 local l1
 local function g()
-	l1 = debug.getinfo(1, "l").currentline; y()
+  l1 = debug.getinfo(1, "l").currentline; y()
 end
-
 local _, stackmsg = xpcall(g, debug.traceback)
 local stack = {}
 for line in string.gmatch(stackmsg, "[^\n]*") do
-	local curr = string.match(line, ":(%d+):")
-	if curr then table.insert(stack, tonumber(curr)) end
+  local curr = string.match(line, ":(%d+):")
+  if curr then table.insert(stack, tonumber(curr)) end
 end
-local i = 1
+local i=1
 while stack[i] ~= l1 do
-	assert(stack[i] == l)
-	i = i + 1
+  assert(stack[i] == l)
+  i = i+1
 end
 assert(i > 15)
 
@@ -176,51 +171,48 @@ assert(i > 15)
 local res, msg = xpcall(error, error)
 assert(not res and type(msg) == 'string')
 
-local function f(x)
-	if x == 0 then error('a\n')
-	else
-		local aux = function() return f(x - 1) end
-		local a, b = xpcall(aux, aux)
-		return a, b
-	end
+local function f (x)
+  if x==0 then error('a\n')
+  else
+    local aux = function () return f(x-1) end
+    local a,b = xpcall(aux, aux)
+    return a,b
+  end
 end
-
 f(3)
 
 -- non string messages
-function f() error { msg = 'x' } end
-
-res, msg = xpcall(f, function(r) return { msg = r.msg .. 'y' } end)
+function f() error{msg='x'} end
+res, msg = xpcall(f, function (r) return {msg=r.msg..'y'} end)
 assert(msg.msg == 'xy')
 
 print('+')
---checksyntax("syntax error", "", "error", 1)
---checksyntax("1.000", "", "1.000", 1)
---checksyntax("[[a]]", "", "[[a]]", 1)
---checksyntax("'aa'", "", "'aa'", 1)
+checksyntax("syntax error", "", "error", 1)
+checksyntax("1.000", "", "1.000", 1)
+checksyntax("[[a]]", "", "[[a]]", 1)
+checksyntax("'aa'", "", "'aa'", 1)
 
 -- test 255 as first char in a chunk
---checksyntax("\255a = 1", "", "\255", 1)
+checksyntax("\255a = 1", "", "\255", 1)
 
 doit('I = loadstring("a=9+"); a=3')
-assert(a == 3 and I == nil)
+assert(a==3 and I == nil)
 print('+')
 
 lim = 1000
 if rawget(_G, "_soft") then lim = 100 end
-for i = 1, lim do
-	doit('a = ')
-	doit('a = 4+nil')
+for i=1,lim do
+  doit('a = ')
+  doit('a = 4+nil')
 end
 
 
 -- testing syntax limits
-local function testrep(init, rep)
-	local s = "local a; " .. init .. string.rep(rep, 400)
-	local a, b = loadstring(s)
-	assert(not a and string.find(b, "syntax levels"))
+local function testrep (init, rep)
+  local s = "local a; "..init .. string.rep(rep, 400)
+  local a,b = loadstring(s)
+  assert(not a and string.find(b, "syntax levels"))
 end
-
 testrep("a=", "{")
 testrep("a=", "(")
 testrep("", "a(")
@@ -234,24 +226,24 @@ testrep("a=", "a^")
 
 -- testing other limits
 -- upvalues
-local s = "function foo ()\n  local "
-for j = 1, 70 do
-	s = s .. "a" .. j .. ", "
+local  s = "function foo ()\n  local "
+for j = 1,70 do
+  s = s.."a"..j..", "
 end
-s = s .. "b\n"
-for j = 1, 70 do
-	s = s .. "function foo" .. j .. " ()\n a" .. j .. "=3\n"
+s = s.."b\n"
+for j = 1,70 do
+  s = s.."function foo"..j.." ()\n a"..j.."=3\n"
 end
-local a, b = loadstring(s)
+local a,b = loadstring(s)
 assert(string.find(b, "line 3"))
 
 -- local variables
 s = "\nfunction foo ()\n  local "
-for j = 1, 300 do
-	s = s .. "a" .. j .. ", "
+for j = 1,300 do
+  s = s.."a"..j..", " 
 end
-s = s .. "b\n"
-local a, b = loadstring(s)
+s = s.."b\n"
+local a,b = loadstring(s)
 assert(string.find(b, "line 2"))
 
 
--- src/test/resources/lua5.1/pm.lua
+++ src/test/resources/lua5.1/pm.lua
@@ -1,33 +1,33 @@
 print('testing pattern matching')
 
 function f(s, p)
-	local i, e = string.find(s, p)
-	if i then return string.sub(s, i, e) end
+  local i,e = string.find(s, p)
+  if i then return string.sub(s, i, e) end
 end
 
 function f1(s, p)
-	p = string.gsub(p, "%%([0-9])", function(s) return "%" .. (s + 1) end)
-	p = string.gsub(p, "^(^?)", "%1()", 1)
-	p = string.gsub(p, "($?)$", "()%1", 1)
-	local t = { string.match(s, p) }
-	return string.sub(s, t[1], t[#t] - 1)
+  p = string.gsub(p, "%%([0-9])", function (s) return "%" .. (s+1) end)
+  p = string.gsub(p, "^(^?)", "%1()", 1)
+  p = string.gsub(p, "($?)$", "()%1", 1)
+  local t = {string.match(s, p)}
+  return string.sub(s, t[1], t[#t] - 1)
 end
 
-a, b = string.find('', '') -- empty patterns are tricky
+a,b = string.find('', '')    -- empty patterns are tricky
 assert(a == 1 and b == 0);
-a, b = string.find('alo', '')
+a,b = string.find('alo', '')
 assert(a == 1 and b == 0)
-a, b = string.find('a\0o a\0o a\0o', 'a', 1) -- first position
+a,b = string.find('a\0o a\0o a\0o', 'a', 1)   -- first position
 assert(a == 1 and b == 1)
-a, b = string.find('a\0o a\0o a\0o', 'a\0o', 2) -- starts in the midle
+a,b = string.find('a\0o a\0o a\0o', 'a\0o', 2)   -- starts in the midle
 assert(a == 5 and b == 7)
-a, b = string.find('a\0o a\0o a\0o', 'a\0o', 9) -- starts in the midle
+a,b = string.find('a\0o a\0o a\0o', 'a\0o', 9)   -- starts in the midle
 assert(a == 9 and b == 11)
-a, b = string.find('a\0a\0a\0a\0\0ab', '\0ab', 2); -- finds at the end
+a,b = string.find('a\0a\0a\0a\0\0ab', '\0ab', 2);  -- finds at the end
 assert(a == 9 and b == 11);
-a, b = string.find('a\0a\0a\0a\0\0ab', 'b') -- last position
+a,b = string.find('a\0a\0a\0a\0\0ab', 'b')    -- last position
 assert(a == 11 and b == 11)
-assert(string.find('a\0a\0a\0a\0\0ab', 'b\0') == nil) -- check ending
+assert(string.find('a\0a\0a\0a\0\0ab', 'b\0') == nil)   -- check ending
 assert(string.find('', '\0') == nil)
 assert(string.find('alo123alo', '12') == 4)
 assert(string.find('alo123alo', '^12') == nil)
@@ -58,16 +58,16 @@ assert(f('aaa', '^.-$') == 'aaa')
 assert(f('aabaaabaaabaaaba', 'b.*b') == 'baaabaaabaaab')
 assert(f('aabaaabaaabaaaba', 'b.-b') == 'baaab')
 assert(f('alo xo', '.o$') == 'xo')
-assert(f(' \n isto ï¿½ assim', '%S%S*') == 'isto')
-assert(f(' \n isto ï¿½ assim', '%S*$') == 'assim')
-assert(f(' \n isto ï¿½ assim', '[a-z]*$') == 'assim')
+assert(f(' \n isto é assim', '%S%S*') == 'isto')
+assert(f(' \n isto é assim', '%S*$') == 'assim')
+assert(f(' \n isto é assim', '[a-z]*$') == 'assim')
 assert(f('um caracter ? extra', '[^%sa-z]') == '?')
 assert(f('', 'a?') == '')
-assert(f('ï¿½', 'ï¿½?') == 'ï¿½')
-assert(f('ï¿½bl', 'ï¿½?b?l?') == 'ï¿½bl')
---assert(f('  ï¿½bl', 'ï¿½?b?l?') == '')
+assert(f('á', 'á?') == 'á')
+assert(f('ábl', 'á?b?l?') == 'ábl')
+assert(f('  ábl', 'á?b?l?') == '')
 assert(f('aa', '^aa?a?a') == 'aa')
---assert(f(']]]ï¿½b', '[^]]') == 'ï¿½')
+assert(f(']]]áb', '[^]]') == 'á')
 assert(f("0alo alo", "%x*") == "0a")
 assert(f("alo alo", "%C+") == "alo alo")
 print('+')
@@ -77,27 +77,21 @@ assert(f1('axz123= 4= 4 34', '(.+)=(.*)=%2 %1') == '3= 4= 4 3')
 assert(f1('=======', '^(=*)=%1$') == '=======')
 assert(string.match('==========', '^([=]*)=%1$') == nil)
 
-local function range(i, j)
-	local item = {}
-	while i <= j do
-		item[#item + 1] = i
-		i = i + 1
-	end
-
-	return unpack(item)
+local function range (i, j)
+  if i <= j then
+    return i, range(i+1, j)
+  end
 end
 
 local abc = string.char(range(0, 255));
 
 assert(string.len(abc) == 256)
 
-function strset(p)
-	local res = { s = '' }
-	string.gsub(abc, p, function(c) res.s = res.s .. c end)
-	return res.s
-end
-
-;
+function strset (p)
+  local res = {s=''}
+  string.gsub(abc, p, function (c) res.s = res.s .. c end)
+  return res.s
+end;
 
 assert(string.len(strset('[\200-\210]')) == 11)
 
@@ -116,67 +110,64 @@ assert(string.match("alo xyzK", "(%w+)K") == "xyz")
 assert(string.match("254 K", "(%d*)K") == "")
 assert(string.match("alo ", "(%w*)$") == "")
 assert(string.match("alo ", "(%w+)$") == nil)
-assert(string.find("(ï¿½lo)", "%(ï¿½") == 1)
-local a, b, c, d, e = string.match("ï¿½lo alo", "^(((.).).* (%w*))$")
---assert(a == 'ï¿½lo alo' and b == 'ï¿½l' and c == 'ï¿½' and d == 'alo' and e == nil)
-a, b, c, d = string.match('0123456789', '(.+(.?)())')
+assert(string.find("(álo)", "%(á") == 1)
+local a, b, c, d, e = string.match("âlo alo", "^(((.).).* (%w*))$")
+assert(a == 'âlo alo' and b == 'âl' and c == 'â' and d == 'alo' and e == nil)
+a, b, c, d  = string.match('0123456789', '(.+(.?)())')
 assert(a == '0123456789' and b == '' and c == 11 and d == nil)
 print('+')
 
-assert(string.gsub('ï¿½lo ï¿½lo', 'ï¿½', 'x') == 'xlo xlo')
-assert(string.gsub('alo ï¿½lo  ', ' +$', '') == 'alo ï¿½lo') -- trim
-assert(string.gsub('  alo alo  ', '^%s*(.-)%s*$', '%1') == 'alo alo') -- double trim
+assert(string.gsub('ülo ülo', 'ü', 'x') == 'xlo xlo')
+assert(string.gsub('alo úlo  ', ' +$', '') == 'alo úlo')  -- trim
+assert(string.gsub('  alo alo  ', '^%s*(.-)%s*$', '%1') == 'alo alo')  -- double trim
 assert(string.gsub('alo  alo  \n 123\n ', '%s+', ' ') == 'alo alo 123 ')
-t = "abï¿½ d"
+t = "abç d"
 a, b = string.gsub(t, '(.)', '%1@')
---assert('@' .. a == string.gsub(t, '', '@') and b == 5)
-a, b = string.gsub('abï¿½d', '(.)', '%0@', 2)
-assert(a == 'a@b@ï¿½d' and b == 2)
+assert('@'..a == string.gsub(t, '', '@') and b == 5)
+a, b = string.gsub('abçd', '(.)', '%0@', 2)
+assert(a == 'a@b@çd' and b == 2)
 assert(string.gsub('alo alo', '()[al]', '%1') == '12o 56o')
 assert(string.gsub("abc=xyz", "(%w*)(%p)(%w+)", "%3%2%1-%0") ==
-		"xyz=abc-abc=xyz")
+              "xyz=abc-abc=xyz")
 assert(string.gsub("abc", "%w", "%1%0") == "aabbcc")
 assert(string.gsub("abc", "%w+", "%0%1") == "abcabc")
-assert(string.gsub('ï¿½ï¿½ï¿½', '$', '\0ï¿½ï¿½') == 'ï¿½ï¿½ï¿½\0ï¿½ï¿½')
+assert(string.gsub('áéí', '$', '\0óú') == 'áéí\0óú')
 assert(string.gsub('', '^', 'r') == 'r')
 assert(string.gsub('', '$', 'r') == 'r')
 print('+')
 
 assert(string.gsub("um (dois) tres (quatro)", "(%(%w+%))", string.upper) ==
-		"um (DOIS) tres (QUATRO)")
+            "um (DOIS) tres (QUATRO)")
 
 do
-	local function setglobal(n, v) rawset(_G, n, v) end
-
-	string.gsub("a=roberto,roberto=a", "(%w+)=(%w%w*)", setglobal)
-	assert(_G.a == "roberto" and _G.roberto == "a")
+  local function setglobal (n,v) rawset(_G, n, v) end
+  string.gsub("a=roberto,roberto=a", "(%w+)=(%w%w*)", setglobal)
+  assert(_G.a=="roberto" and _G.roberto=="a")
 end
 
-function f(a, b) return string.gsub(a, '.', b) end
+function f(a,b) return string.gsub(a,'.',b) end
+assert(string.gsub("trocar tudo em |teste|b| é |beleza|al|", "|([^|]*)|([^|]*)|", f) ==
+            "trocar tudo em bbbbb é alalalalalal")
 
-assert(string.gsub("trocar tudo em |teste|b| ï¿½ |beleza|al|", "|([^|]*)|([^|]*)|", f) ==
-		"trocar tudo em bbbbb ï¿½ alalalalalal")
-
-local function dostring(s) return loadstring(s)() or "" end
-
+local function dostring (s) return loadstring(s)() or "" end
 assert(string.gsub("alo $a=1$ novamente $return a$", "$([^$]*)%$", dostring) ==
-		"alo  novamente 1")
+            "alo  novamente 1")
 
 x = string.gsub("$x=string.gsub('alo', '.', string.upper)$ assim vai para $return x$",
-	"$([^$]*)%$", dostring)
+         "$([^$]*)%$", dostring)
 assert(x == ' assim vai para ALO')
 
 t = {}
 s = 'a alo jose  joao'
-r = string.gsub(s, '()(%w+)()', function(a, w, b)
-	assert(string.len(w) == b - a);
-	t[a] = b - a;
-end)
+r = string.gsub(s, '()(%w+)()', function (a,w,b)
+      assert(string.len(w) == b-a);
+      t[a] = b-a;
+    end)
 assert(s == r and t[1] == 1 and t[3] == 3 and t[7] == 4 and t[13] == 4)
 
 
-function isbalanced(s)
-	return string.find(string.gsub(s, "%b()", ""), "[()]") == nil
+function isbalanced (s)
+  return string.find(string.gsub(s, "%b()", ""), "[()]") == nil
 end
 
 assert(isbalanced("(9 ((8))(\0) 7) \0\0 a b ()(c)() a"))
@@ -184,17 +175,17 @@ assert(not isbalanced("(9 ((8) 7) a b (\0 c) a"))
 assert(string.gsub("alo 'oi' alo", "%b''", '"') == 'alo " alo')
 
 
-local t = { "apple", "orange", "lime"; n = 0 }
-assert(string.gsub("x and x and x", "x", function() t.n = t.n + 1; return t[t.n] end)
-		== "apple and orange and lime")
+local t = {"apple", "orange", "lime"; n=0}
+assert(string.gsub("x and x and x", "x", function () t.n=t.n+1; return t[t.n] end)
+        == "apple and orange and lime")
 
-t = { n = 0 }
-string.gsub("first second word", "%w%w*", function(w) t.n = t.n + 1; t[t.n] = w end)
+t = {n=0}
+string.gsub("first second word", "%w%w*", function (w) t.n=t.n+1; t[t.n] = w end)
 assert(t[1] == "first" and t[2] == "second" and t[3] == "word" and t.n == 3)
 
-t = { n = 0 }
+t = {n=0}
 assert(string.gsub("first second word", "%w+",
-	function(w) t.n = t.n + 1; t[t.n] = w end, 2) == "first second word")
+         function (w) t.n=t.n+1; t[t.n] = w end, 2) == "first second word")
 assert(t[1] == "first" and t[2] == "second" and t[3] == nil)
 
 assert(not pcall(string.gsub, "alo", "(.", print))
@@ -211,8 +202,8 @@ assert(not string.find(a, '^a*.?b$'))
 assert(string.find(a, '^a-.?$'))
 
 -- deep nest of gsubs
-function rev(s)
-	return string.gsub(s, "(.)(.+)", function(c, s1) return rev(s1) .. c end)
+function rev (s)
+  return string.gsub(s, "(.)(.+)", function (c,s1) return rev(s1)..c end)
 end
 
 local x = string.rep('012345', 10)
@@ -221,40 +212,40 @@ assert(rev(rev(x)) == x)
 
 -- gsub with tables
 assert(string.gsub("alo alo", ".", {}) == "alo alo")
-assert(string.gsub("alo alo", "(.)", { a = "AA", l = "" }) == "AAo AAo")
-assert(string.gsub("alo alo", "(.).", { a = "AA", l = "K" }) == "AAo AAo")
-assert(string.gsub("alo alo", "((.)(.?))", { al = "AA", o = false }) == "AAo AAo")
+assert(string.gsub("alo alo", "(.)", {a="AA", l=""}) == "AAo AAo")
+assert(string.gsub("alo alo", "(.).", {a="AA", l="K"}) == "AAo AAo")
+assert(string.gsub("alo alo", "((.)(.?))", {al="AA", o=false}) == "AAo AAo")
 
-assert(string.gsub("alo alo", "().", { 2, 5, 6 }) == "256 alo")
+assert(string.gsub("alo alo", "().", {2,5,6}) == "256 alo")
 
-t = {}; setmetatable(t, { __index = function(t, s) return string.upper(s) end })
+t = {}; setmetatable(t, {__index = function (t,s) return string.upper(s) end})
 assert(string.gsub("a alo b hi", "%w%w+", t) == "a ALO b HI")
 
 
 -- tests for gmatch
 assert(string.gfind == string.gmatch)
 local a = 0
-for i in string.gmatch('abcde', '()') do assert(i == a + 1); a = i end
---assert(a == 6) -- Currently 5
+for i in string.gmatch('abcde', '()') do assert(i == a+1); a=i end
+assert(a==6)
 
-t = { n = 0 }
+t = {n=0}
 for w in string.gmatch("first second word", "%w+") do
-	t.n = t.n + 1; t[t.n] = w
+      t.n=t.n+1; t[t.n] = w
 end
 assert(t[1] == "first" and t[2] == "second" and t[3] == "word")
 
-t = { 3, 6, 9 }
-for i in string.gmatch("xuxx uu ppar r", "()(.)%2") do
-	assert(i == table.remove(t, 1))
+t = {3, 6, 9}
+for i in string.gmatch ("xuxx uu ppar r", "()(.)%2") do
+  assert(i == table.remove(t, 1))
 end
 assert(table.getn(t) == 0)
 
 t = {}
-for i, j in string.gmatch("13 14 10 = 11, 15= 16, 22=23", "(%d+)%s*=%s*(%d+)") do
-	t[i] = j
+for i,j in string.gmatch("13 14 10 = 11, 15= 16, 22=23", "(%d+)%s*=%s*(%d+)") do
+  t[i] = j
 end
 a = 0
-for k, v in pairs(t) do assert(k + 1 == v + 0); a = a + 1 end
+for k,v in pairs(t) do assert(k+1 == v+0); a=a+1 end
 assert(a == 3)
 
 
@@ -272,9 +263,9 @@ assert(i == 2 and e == 5)
 local k = string.match(" alo aalo allo", "%f[%S](.-%f[%s].-%f[%S])")
 assert(k == 'alo ')
 
-local a = { 1, 5, 9, 14, 17, }
+local a = {1, 5, 9, 14, 17,}
 for k in string.gmatch("alo alo th02 is 1hat", "()%f[%w%d]") do
-	assert(table.remove(a, 1) == k)
+  assert(table.remove(a, 1) == k)
 end
 assert(table.getn(a) == 0)
 
