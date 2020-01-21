92d81faf79d1688e831bfc662125736b4ba35c26
SquidDev <bonzoweb@hotmail.co.uk>
2020-01-21 19:10:22 +0000

    Import Lua 5.3 tests

---
+++ src/test/java/org/squiddev/cobalt/Lua53Tests.java
@@ -0,0 +1,91 @@
+package org.squiddev.cobalt;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.squiddev.cobalt.compiler.CompileException;
+import org.squiddev.cobalt.function.OneArgFunction;
+import org.squiddev.cobalt.function.ZeroArgFunction;
+import org.squiddev.cobalt.lib.Bit32Lib;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.squiddev.cobalt.ValueFactory.valueOf;
+
+/**
+ * Lua driven assertion tests
+ */
+@RunWith(Parameterized.class)
+public class Lua53Tests {
+	private final String name;
+	private ScriptDrivenHelpers helpers;
+
+	public Lua53Tests(String name) {
+		this.name = name;
+		this.helpers = new ScriptDrivenHelpers("/lua5.3/");
+	}
+
+	@Parameterized.Parameters(name = "{0}")
+	public static Collection<Object[]> getTests() {
+		Object[][] tests = {
+			{"all"},
+			{"api"},
+			{"attrib"},
+			{"big"},
+			{"bitwise"},
+			{"calls"},
+			{"closure"},
+			{"code"},
+			{"constructs"},
+			{"coroutine"},
+			{"db"},
+			{"errors"},
+			{"events"},
+			{"files"},
+			{"gc"},
+			{"goto"},
+			{"literals"},
+			{"locals"},
+			{"main"}, // Tests Lua flags
+			{"math"},
+			{"nextvar"},
+			{"pm"},
+			{"sort"},
+			{"strings"},
+			{"tpack"},
+			{"utf8"},
+			{"vararg"},
+			{"verybig"},
+		};
+
+		return Arrays.asList(tests);
+	}
+
+	@Before
+	public void setup() throws LuaError, UnwindThrowable {
+		helpers.setup();
+		helpers.globals.load(helpers.state, new Bit32Lib());
+		helpers.globals.rawset("mkdir", new OneArgFunction() {
+			@Override
+			public LuaValue call(LuaState state, LuaValue arg) throws LuaError {
+				return valueOf(new File(arg.checkString()).mkdirs());
+			}
+		});
+
+		OperationHelper.setTable(helpers.state, helpers.globals.rawget("debug"), valueOf("debug"), new ZeroArgFunction() {
+			@Override
+			public LuaValue call(LuaState state) {
+				return Constants.NONE;
+			}
+		});
+	}
+
+	@Test
+	public void run() throws IOException, CompileException, LuaError, InterruptedException {
+		LuaThread.runMain(helpers.state, helpers.loadScript(name));
+	}
+}
