6144bbe7bbd110b6fb6093acb8b47f018aea0b75
SquidDev <bonzoweb@hotmail.co.uk>
2020-01-23 17:49:58 +0000

    Add utf8 test suite

--- src/test/java/org/squiddev/cobalt/AssertTests.java
+++ src/test/java/org/squiddev/cobalt/AssertTests.java
@@ -24,12 +24,14 @@
  */
 package org.squiddev.cobalt;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.squiddev.cobalt.compiler.CompileException;
 import org.squiddev.cobalt.function.OneArgFunction;
 import org.squiddev.cobalt.function.ZeroArgFunction;
 import org.squiddev.cobalt.lib.Bit32Lib;
+import org.squiddev.cobalt.lib.Utf8Lib;
 
 import java.io.File;
 import java.io.IOException;
@@ -56,13 +58,13 @@ public class AssertTests {
 	public void main(String name) throws IOException, CompileException, LuaError, InterruptedException {
 		ScriptHelper helpers = new ScriptHelper("/assert/");
 		helpers.setup();
-		LuaThread.runMain(helpers.state, helpers.loadScript(name));
+		helpers.runWithDump(name);
 	}
 
 	@ParameterizedTest(name = ParameterizedTest.ARGUMENTS_WITH_NAMES_PLACEHOLDER)
 	@ValueSource(strings = {
 		// Skip all, api, big and main
-		"attrib", "bitwise",
+		"attrib",
 		"calls",
 		"checktable",
 		"closure",
@@ -86,7 +88,6 @@ public class AssertTests {
 	public void lua51(String name) throws Exception {
 		ScriptHelper helpers = new ScriptHelper("/assert/lua5.1/");
 		helpers.setup();
-		helpers.globals.load(helpers.state, new Bit32Lib());
 		helpers.globals.rawset("mkdir", new OneArgFunction() {
 			@Override
 			public LuaValue call(LuaState state, LuaValue arg) throws LuaError {
@@ -102,6 +103,66 @@ public class AssertTests {
 			}
 		});
 
-		LuaThread.runMain(helpers.state, helpers.loadScript(name));
+		helpers.runWithDump(name);
+	}
+
+	@ParameterizedTest(name = ParameterizedTest.ARGUMENTS_WITH_NAMES_PLACEHOLDER)
+	@ValueSource(strings = {
+		"bitwise",
+	})
+	public void lua52(String name) throws Exception {
+		ScriptHelper helpers = new ScriptHelper("/assert/lua5.2/");
+		helpers.setup();
+		helpers.globals.load(helpers.state, new Bit32Lib());
+
+		helpers.runWithDump(name);
+	}
+
+	@ParameterizedTest(name = ParameterizedTest.ARGUMENTS_WITH_NAMES_PLACEHOLDER)
+	@ValueSource(strings = {
+		"utf8",
+	})
+	public void lua53(String name) throws Exception {
+		ScriptHelper helpers = new ScriptHelper("/assert/lua5.3/");
+		helpers.setup();
+		helpers.globals.load(helpers.state, new Utf8Lib());
+
+		helpers.runWithDump(name);
+	}
+
+	@Test
+	public void foo() {
+		Buffer sb = new Buffer(1);
+		byte[] buffer = new byte[8];
+		// 27721,23383,47,28450,23383
+		int codepoint = 23383;
+		if (codepoint < 0x80) {
+			sb.append((byte) codepoint);
+		} else {
+			int mfb = 0x3f;
+			int j = 1;
+			do {
+				buffer[8 - j++] = ((byte) (0x80 | (codepoint & 0x3f)));
+				codepoint >>= 6;
+				mfb >>= 1;
+			} while (codepoint > mfb);
+			buffer[8 - j] = (byte) ((~mfb << 1) | codepoint);
+			sb.append(buffer, 8 - j, j);
+		}
+
+		// 27721: 230     177     137
+		// 23383: 229     173     151
+		// 47   : 47
+		// 28450: 230     188     162
+		// 23383: 229     173     151
+		for (int i : buffer) System.out.printf("%d ", i & 0xFF);
+		System.out.println();
+
+		LuaString st = sb.toLuaString();
+		for (int i = 0; i < st.length; i++) System.out.printf("%d ", st.luaByte(i));
+		System.out.println();
+
+		System.out.println("\u5b57");
+		System.out.println(st.toString());
 	}
 }
